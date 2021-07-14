2b10ce516cb0d739f2bedb79cd761ab9327ab820
Sandeep Pramanik <sandeep.pramanik@met-technologies.com>
2021-07-14 19:01:33 +0530

    Upto homepage

---
+++ app/src/main/java/com/example/akaya/ui/login/CreatePasswordActivity.kt
@@ -0,0 +1,13 @@
+package com.example.akaya.ui.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.akaya.R
+
+class CreatePasswordActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_password)
+    }
+}
